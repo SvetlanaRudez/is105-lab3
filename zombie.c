f1103eb9928ecce6173b70850aac434c24ea316f
Svetlana Rudez <wls@LAPTOP-FNK17HHU.localdomain>
2021-05-03 19:51:03 +0200

    Endret koden

--- zombie.c
+++ zombie.c
@@ -1,26 +1,25 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
-    int pid = fork();    // fork a child process
+       pid_t pid = fork(); // fork a child process
 
-    if(pid > 0)    //if pid greater than 0 - parent process
-
-    {
-        printf("This is the parent process.\n");
-        sleep(10);  //to create zombie process we can’t use wait(), but we can use sleep() systemcall.
-
-}
-    else if (pid == 0)    //if pid equal 0 - child process
-
+    if (pid > 0) //if pid greater than 0 - parent process
 {
-        printf("This is the child process\n");
-        exit(0);   // terminating the child process
-
+       sleep(20); //to create zombie process we can't use wait(), but we can use sleep() systemcall
+       printf("This is the parent process\n");
 
     }
+    else if(pid == 0) //if pid equal 0 - child process
+{
+            printf("This is the child process\n");
+            exit(0); // terminating the child process
+   }
+
     return 0;
 }
+
+
